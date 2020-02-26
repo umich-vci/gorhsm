803e861720130a9d23707a7aae8a54afaf22e0e2
Adam Robinson <adarobin@umich.edu>
2020-02-26 14:47:42 -0500

    add function to get an access token

---
+++ generate_access_token.go
@@ -0,0 +1,86 @@
+package gorhsm
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+const rhsmTokenURL = "https://sso.redhat.com/auth/realms/redhat-external/protocol/openid-connect/token"
+
+// Token describes the response from RHSM token refresh requests
+type Token struct {
+	AccessToken      string `json:"access_token"`
+	ExpiresIn        int    `json:"expires_in"`
+	NotBeforePolicy  int    `json:"not-before-policy"`
+	RefreshExpiresIn int    `json:"refresh_expires_in"`
+	RefreshToken     string `json:"refresh_token"`
+	Scope            string `json:"scope"`
+	SessionState     string `json:"session_state"`
+	TokenType        string `json:"token_type"`
+}
+
+type tokenErrorResponse struct {
+	// HTTP response that caused this error
+	Response *http.Response
+
+	// Error message
+	ErrorMsg         string `json:"error"`
+	ErrorDescription string `json:"error_description"`
+}
+
+// GenerateAccessToken generats a new API bearer token
+func GenerateAccessToken(refreshToken string) (*Token, error) {
+	req, err := http.NewRequest(http.MethodGet, rhsmTokenURL, nil)
+	if err != nil {
+		return nil, err
+	}
+	req.Form.Add("grant_type", "refresh_token")
+	req.Form.Add("client_id", "rhsm-api")
+	req.Form.Add("refresh_token", refreshToken)
+
+	client := new(http.Client)
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	err = checkTokenResponse(resp)
+	if err != nil {
+		return nil, err
+	}
+
+	token := new(Token)
+
+	err = json.NewDecoder(resp.Body).Decode(token)
+	if err != nil {
+		return nil, fmt.Errorf("error decoding token response body")
+	}
+
+	return token, nil
+
+}
+
+func checkTokenResponse(r *http.Response) error {
+	if c := r.StatusCode; c >= 200 && c <= 299 {
+		return nil
+	}
+
+	errorResponse := &tokenErrorResponse{Response: r}
+	data, err := ioutil.ReadAll(r.Body)
+	if err == nil && len(data) > 0 {
+		err := json.Unmarshal(data, errorResponse)
+		if err != nil {
+			errorResponse.ErrorMsg = string(data)
+		}
+	}
+
+	return errorResponse
+}
+
+func (r *tokenErrorResponse) Error() string {
+	return fmt.Sprintf("%v %v: %d %v",
+		r.Response.Request.Method, r.Response.Request.URL, r.Response.StatusCode, r.ErrorMsg+": "+r.ErrorDescription)
+}
